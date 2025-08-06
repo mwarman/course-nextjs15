57d9b72146f336286048c82e109928492812c56d
Matthew Warman <warmanmr@gmail.com>
2025-08-06 10:33:28 -0400

    Refactor MainHeader to use Next.js Image component for logo

--- foodies-app/components/main-header.tsx
+++ foodies-app/components/main-header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import classes from './main-header.module.css';
 import logoImg from '@/assets/logo.png';
@@ -7,7 +8,7 @@ const MainHeader = () => {
   return (
     <header className={classes.header}>
       <Link href="/" className={classes.logo}>
-        <img src={logoImg.src} alt="A plate with food on it" />
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
 
