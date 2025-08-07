eb407ae63c0cd5cfd0da2ac4a619a6daaa555f5a
Matthew Warman <warmanmr@gmail.com>
2025-08-07 11:02:14 -0400

    feat: add loading component for Meals page with styles

---
+++ foodies-app/app/meals/loading.tsx
@@ -0,0 +1,7 @@
+import classes from './loading.module.css';
+
+const MealsPageLoading = () => {
+  return <p className={classes.loading}>Loading meals...</p>;
+};
+
+export default MealsPageLoading;
